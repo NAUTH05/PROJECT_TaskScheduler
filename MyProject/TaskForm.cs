a5beeb99914dadb7ec54a8987d43e83b92aeebb1
NAUTH05 <nguyenthuan.dev05@gmail.com>
2025-11-07 13:23:36 +0700

    Rm API from repository

--- MyProject/TaskForm.cs
+++ MyProject/TaskForm.cs
@@ -204,6 +204,9 @@ namespace MyProject
             public string ProjectID { get; set; }
             public string TaskName { get; set; }
             public string TaskDescription { get; set; }
+
+
+
             public string DueDate { get; set; }
             public string Priority { get; set; }
             public string Status { get; set; }
